b7a40da7a747adf363f8d228dc6673cf61c9af43
AnastasiiaYaM <melenevych@ukr.net>
2022-09-24 01:53:14 +0300

    Updates apiKey

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "3a94f3778290bfeee61278505dbbe51d";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
